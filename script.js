008ac992c9871bc92c26c519622b5cccd4f4afae
Jessica Perez <1jessicaperez@gmail.com>
2020-10-02 15:22:36 -0400

    easy and hard mode added

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-//Array of colors
-var colors = generateRandomColors(6);
+//INITIALIZE VARIABLES
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
+var pickedColor = pickColor();
+
 //DOM ELEMENTS
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-//INITIALIZE VARIABLES
-var pickedColor = pickColor();
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
+easyBtn.addEventListener("click", function () {
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  //generate new colors
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  //adjust display
+  colorDisplay.textContent = pickedColor;
+  //change color of 3 top squares
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+hardBtn.addEventListener("click", function () {
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  //generate new colors
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  //adjust display
+  colorDisplay.textContent = pickedColor;
+  //change color of 3 top squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+});
 //RESET COLORS
 resetButton.addEventListener("click", function () {
   //generate new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   //pick a new random color from array
   pickedColor = pickColor();
   //change color display message to match picked color
@@ -44,6 +80,7 @@ for (var i = 0; i < squares.length; i++) {
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try again";
+      resetButton.textContent = "New Colors";
     }
   });
 }
