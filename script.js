d036f6967cb751164893cb6c311ecc6247104108
Jessica Perez <1jessicaperez@gmail.com>
2020-10-02 13:39:33 -0400

    Added basic color logic and click event

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-alert("connected");
+//Array of colors
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)",
+];
+//DOM ELEMENTS
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+//INITIALIZE VARIABLES
+var pickedColor = colors[3];
+
+//UPDATE RGB TEXT CONTENT DISPLAY
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  //add initial colors to squares
+  squares[i].style.backgroundColor = colors[i];
+  //add click listeners to squares
+  squares[i].addEventListener("click", function () {
+    //grab color of picked square
+    var clickedColor = this.style.backgroundColor;
+    //compare color to picked color
+    if (clickedColor === pickedColor) {
+      alert("correct");
+    } else {
+      this.style.backgroundColor = "#232323";
+    }
+  });
+}
